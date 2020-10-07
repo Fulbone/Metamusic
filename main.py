3216b9ef5981c20bffb52651af402487c7a3c3fe
Jake <47907450+Fulbone@users.noreply.github.com>
2020-10-07 18:39:30 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import kivy
 kivy.require('1.11.1')
 from kivy.app import App 
-from kivy.uix.label import Label 
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button 
+from kivy.uix.screenmanager import ScreenManager, Screen 
 
-class BandScreen(BoxLayout):
+class WindowManager(ScreenManager):
 	pass
 		
-		
+class ScreenWrapper(Screen):
+	pass
 
+class Comparison(ScreenWrapper, Screen):
+	pass
 
+class Analysis(ScreenWrapper, Screen):
+	pass
+
+class Tuner(ScreenWrapper, Screen):
+	pass
+
+class Metronome(ScreenWrapper, Screen):
+	pass
 
 class BandApp(App):
 	def build(self):
-		return BandScreen()
-
+		pass
 
 
 if __name__ == '__main__':
